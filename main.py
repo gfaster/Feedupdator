9b4690e32a2e8fc78f8e0d104b83576e70d47b4e
Gavin Rohrer <gavin@rohrer.org>
2021-08-19 22:37:59 -0400

    Matching improvements, now checks if the series already exists first

--- main.py
+++ main.py
@@ -8,26 +8,33 @@ import feedparser
 from datetime import datetime as dt
 from datetime import timedelta
 import asyncio
+from fuzzy_match import algorithims
+from dataclasses import dataclass
 
 config = dotenv_values('.env')
 database = Database("sqlite:///{0}".format(config['DATABASE']))
 
-
-bot = commands.Bot(command_prefix='$wm ')
+command_prefix = '$wm '
+bot = commands.Bot(command_prefix=command_prefix)
 
 
 async def db_setup():
 	await database.connect()
-	q1 = "CREATE TABLE IF NOT EXISTS ChannelFollows (id INTEGER PRIMARY KEY NOT null, channel INTEGER , keyword VARCHAR(1023));"
-	q2 = "CREATE INDEX IF NOT EXISTS idx_keyword ON ChannelFollows (keyword);"
-	q3 = "CREATE UNIQUE INDEX IF NOT EXISTS idx_channel_keyword ON ChannelFollows (channel, keyword);"
+	q8 = "CREATE TABLE IF NOT EXISTS Series (id INTEGER PRIMARY KEY NOT null, full_title VARCHAR(1023), platform VARCHAR(255))"
+	q1 = """CREATE TABLE IF NOT EXISTS ChannelFollows (
+			id INTEGER PRIMARY KEY NOT null, channel INTEGER , series_id INTEGER, FOREIGN KEY(series_id) REFERENCES Series(id));"""
+	q2 = "CREATE INDEX IF NOT EXISTS idx_series_id ON ChannelFollows (series_id);"
+	q3 = "CREATE UNIQUE INDEX IF NOT EXISTS idx_channel_series_id ON ChannelFollows (channel, series_id);"
 	q4 = "CREATE TABLE IF NOT EXISTS Refresh (id INTEGER PRIMARY KEY NOT null, provider VARCHAR(255), last_refresh DATETIME, etag VARCHAR(255), modified VARCHAR(255))"
 	q5 = "CREATE TABLE IF NOT EXISTS PrevSends (id INTEGER PRIMARY KEY NOT null, channel INTEGER, permalink VARCHAR(1023))"
 	q6 = "CREATE UNIQUE INDEX IF NOT EXISTS idx_channel_permalink ON PrevSends (channel, permalink);"
-	q6 = "CREATE UNIQUE INDEX IF NOT EXISTS idx_provider ON Refresh (provider);"
+	q7 = "CREATE UNIQUE INDEX IF NOT EXISTS idx_provider ON Refresh (provider);"
 
 
-	queries = [q1, q2, q3, q4, q5, q6]
+
+
+
+	queries = [q8, q1, q2, q3, q4, q5, q6, q7]
 	for query in queries:
 		await database.execute(query=query)
 	
@@ -44,9 +51,6 @@ def san(text):
 	return re.sub(r'[()";]', '', text)
 
 
-async def _add(channel_id, keyword):
-	pass
-
 @bot.command(help='add a follow for this channel')
 async def add(ctx, *args):
 	keyword = san(' '.join(args))
@@ -54,13 +58,18 @@ async def add(ctx, *args):
 		await ctx.send('too long!')
 		return
 
-	query = "INSERT INTO ChannelFollows(channel, keyword) VALUES (:channel, :keyword)"
-	values = {'channel': ctx.channel.id, 'keyword':keyword}
-	try:
-		await database.execute(query=query, values=values)
-		await ctx.send('added **{0}** to list of followed titles'.format(keyword))
-	except IntegrityError as e:
-		await ctx.send('**{0}** is already followed'.format(keyword))
+	series_match = await findSeries(keyword)
+
+	if series_match.distance > 0.8:
+		query = "INSERT INTO ChannelFollows(channel, series_id) VALUES (:channel, :series_id)"
+		values = {'channel': ctx.channel.id, 'series_id':series_match.index}
+		try:
+			await database.execute(query=query, values=values)
+			await ctx.send('added **{0}** to list of followed titles'.format(series_match.title))
+		except IntegrityError as e:
+			await ctx.send('**{0}** is already followed'.format(series_match.title))
+	else:
+		await ctx.send("couldn't find **{0}**, did you mean **{1}**?".format(keyword, series_match.title))
 	
 
 @bot.command(help='remove a follow for this channel')
@@ -70,17 +79,21 @@ async def remove(ctx, *args):
 		await ctx.send('too long!')
 		return
 
-	query = "SELECT * FROM ChannelFollows WHERE channel=:channel AND keyword=:keyword LIMIT 1"
-	values = {'channel': ctx.channel.id, 'keyword':keyword}
-	exists = await database.fetch_all(query=query, values=values)
-	if len(exists) == 0:
-		await ctx.send('**{0}** was never followed'.format(keyword))
-		return
+	series_match = await findSeries(keyword)
+	if series_match.distance > 0.8:
+		query = "SELECT * FROM ChannelFollows WHERE channel=:channel AND series_id=:series_id LIMIT 1"
+		values = {'channel': ctx.channel.id, 'series_id':series_match.index}
+		exists = await database.fetch_all(query=query, values=values)
+		if len(exists) == 0:
+			await ctx.send('**{0}** was never followed, type `{1} list` to see a list of followed series '.format(keyword, command_prefix))
+			return
 
-	query = "DELETE FROM ChannelFollows WHERE channel=:channel AND keyword=:keyword"
-	
-	await database.execute(query=query, values=values)
-	await ctx.send('removed **{0}**'.format(keyword))
+		query = "DELETE FROM ChannelFollows WHERE channel=:channel AND series_id=:series_id"
+		
+		await database.execute(query=query, values=values)
+		await ctx.send('removed **{0}**'.format(series.title))
+	else:
+		await ctx.send("couldn't find **{0}**, did you mean **{1}**?".format(keyword, series_match.title))
 
 
 @bot.command(help='Clear all follows for this channel')
@@ -93,11 +106,18 @@ async def clear(ctx):
 
 @bot.command(help='List all follows for this channel')
 async def list(ctx):
-	query = """SELECT keyword FROM ChannelFollows WHERE channel=:channel LIMIT 100"""
+	query = """SELECT Series.full_title 
+	FROM Series 
+	INNER JOIN ChannelFollows ON Series.id=ChannelFollows.series_id
+	WHERE ChannelFollows.channel=:channel LIMIT 100"""
+
 	values = {'channel': ctx.channel.id}
+
 	co_keywords = await database.fetch_all(query=query, values=values)
 	str_keywords = '\n'.join([x[0] for x in co_keywords])
-	await ctx.send('followed tags for <#{0}>:\n{1}'.format( str(ctx.channel.id), str_keywords ))
+	await ctx.send('followed series for <#{0}>:\n{1}'.format( str(ctx.channel.id), str_keywords ))
+
+
 
 # returns a feed and updates db if enough time has passed since last retry
 async def updateRefresh(provider):
@@ -120,11 +140,15 @@ async def updateRefresh(provider):
 	prev_check_time = dt.strptime(last_refresh[2], "%Y-%m-%d %H:%M:%S") - timedelta(hours = 4)
 	time_elapsed = (dt.now() - prev_check_time).total_seconds()
 	
-	if time_elapsed < int(config['REFRESH_SLEEP']):
+	if time_elapsed < int(config['REFRESH_SLEEP']) - 3:
+		print('just tried to refresh, but not enough time has passed')
 		return None
 
 
-	d = feedparser.parse(provider, etag=last_refresh[3], modified=last_refresh[4])
+	# Get a new feed and update the database
+
+	# d = feedparser.parse(provider, etag=last_refresh[3], modified=last_refresh[4])
+	d = feedparser.parse(provider, modified=last_refresh[4])
 	if int(d.status) == 200:
 		etag = getattr(d, 'etag', None)
 		modified = getattr(d, 'modified', None)
@@ -133,9 +157,24 @@ async def updateRefresh(provider):
 		values = {"etag": etag, 'modified': modified, 'provider':provider}
 		await database.execute(query=query, values=values)
 
+		# update series if there are any
+		for entry in d.entries:
+			assert 'crunchyroll_seriestitle' in entry
+			series_match = await findSeries(entry['crunchyroll_seriestitle'])
+			if series_match.distance < 0.95:
+				# this is a different series, lets add it
+				query = "INSERT INTO Series(full_title, platform) VALUES (:full_title, 'Crunchyroll')"
+				values = {'full_title': entry['crunchyroll_seriestitle']}
+				await database.execute(query=query, values=values)
+				print('\tJust added {0} to the series list'.format(entry['crunchyroll_seriestitle']))
+
+
+
 		return d
 	else:
-		# TODO: Make a cache here
+		query = "UPDATE Refresh SET last_refresh = datetime('now') WHERE provider=:provider"
+		values = {'provider':provider}
+		await database.execute(query=query, values=values)
 		return None
 
 async def hasSent(channel, permalink):
@@ -147,12 +186,16 @@ async def hasSent(channel, permalink):
 async def sendNewShows(feed):
 	for item in feed.entries:
 
-		query = "SELECT * FROM ChannelFollows WHERE keyword=:keyword"
-		values = {'keyword':item['crunchyroll_seriestitle']}
-		rows_to_send = await database.fetch_all(query=query, values=values)
-		for row in rows_to_send:
+		current_show = await findSeries(item['crunchyroll_seriestitle'])
+		assert current_show.index > 0.99
+		query = "SELECT * FROM ChannelFollows WHERE series_id=:series_id"
+		values = {'series_id':current_show.index}
+		followed_rows = await database.fetch_all(query=query, values=values)
+
+		for row in followed_rows:
 			if not await hasSent(row[1], item['feedburner_origlink']):
 				channel = bot.get_channel(row[1])
+				print('\tsending {0} to #{1}'.format(item['title'], channel.name))
 
 				title = '**{0}** is now out!'.format(item['title'])
 				embed = discord.Embed(title=title, url=item['feedburner_origlink'])
@@ -164,6 +207,34 @@ async def sendNewShows(feed):
 				await database.execute(query=query, values=values)
 
 
+@dataclass
+class Match:
+	index: int
+	distance: float
+	title: str
+
+#fuzzy searches the series table to find a show, returns a Match object for the best match
+async def findSeries(series_name):
+	# See if there is an exact match
+	query = "SELECT * FROM Series WHERE full_title=:series_name"
+	values = {'series_name': series_name}
+	exact = await database.fetch_all(query=query, values=values)
+	if len(exact) > 0:
+		return Match(exact[0][0], 1.0, series_name)
+
+	# I haven't found a great way of doing this since I can't fuzzy WHERE easily
+	all_series = await database.fetch_all(query="SELECT * FROM Series")
+
+	best_match = None
+
+	for series in all_series:
+		match_distance = algorithims.levenshtein(series_name.lower(), series[1].lower())
+		if best_match is None or best_match.distance < match_distance:
+			best_match = Match(series[0], match_distance, series[1])
+
+
+	return best_match
+
 
 
 
@@ -173,8 +244,8 @@ async def refresh(ctx):
 
 	if feed is not None:
 		# await ctx.send('Updated!')
-		await sendNewShows(feed)
 		print(str(dt.now()) + " Updated, send some new shows")
+		await sendNewShows(feed)
 	else:
 		print(str(dt.now()) + " Updated, found nothing")
 		# await ctx.send('Nothing new :(')
